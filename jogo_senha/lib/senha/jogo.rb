30293582c03bd40c7325dc4c13409fa29784bc67
Anderson Marinho da Silva <anmdss@gmail.com>
2013-12-07 10:35:10 -0300

    Método iniciar.

--- jogo_senha/lib/senha/jogo.rb
+++ jogo_senha/lib/senha/jogo.rb
@@ -1,4 +1,12 @@
 module Senha
 	class Jogo
+		def initialize(output)
+			@output = output
+		end
+
+		def iniciar
+			@output.puts "Bem-Vindo ao Senha"
+			@output.puts "Informe o jogador:"
+		end
 	end
 end
--- jogo_senha/spec/senha/jogo_spec.rb
+++ jogo_senha/spec/senha/jogo_spec.rb
@@ -4,8 +4,25 @@ require 'spec_helper'
 module Senha
 	describe Jogo do
 		describe "#iniciar" do
-			it "enviar uma mensagem de boas-vindas"
-			it "solicita pela 1.o jogador"
+			it "enviar uma mensagem de boas-vindas" do
+				output = double('output').as_null_object
+
+				jogo = Jogo.new(output)
+
+				output.should_receive(:puts).with("Bem-Vindo ao Senha")
+
+				jogo.iniciar
+			end
+
+			it "solicita pela 1.o jogador" do 
+				output = double('output').as_null_object
+
+				jogo = Jogo.new(output)
+
+				output.should_receive(:puts).with("Informe o jogador:")
+
+				jogo.iniciar
+			end
 		end
 	end
 end

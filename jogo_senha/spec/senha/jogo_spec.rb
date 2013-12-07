a57a7e52799dbe976f612d5235566aa50d3d8bed
Anderson Marinho da Silva <anmdss@gmail.com>
2013-12-07 10:52:21 -0300

    Alternativa ao before para iniciação de variáveis comuns.

--- jogo_senha/spec/senha/jogo_spec.rb
+++ jogo_senha/spec/senha/jogo_spec.rb
@@ -4,21 +4,22 @@ require 'spec_helper'
 module Senha
 	describe Jogo do
 		describe "#iniciar" do
+			# alternativa ao before, mas que apenas inicia as variáveis quando necessário
+			let(:output) { double('output').as_null_object }
+			let(:jogo) { Jogo.new(output) }
+
+			#before(:each) do
+			#	@output = double('output').as_null_object
+			#	@jogo = Jogo.new(@output)
+			#end
+
 			it "enviar uma mensagem de boas-vindas" do
-				output = double('output').as_null_object
-
-				jogo = Jogo.new(output)
-
 				output.should_receive(:puts).with("Bem-Vindo ao Senha")
 
 				jogo.iniciar
 			end
 
 			it "solicita pela 1.o jogador" do 
-				output = double('output').as_null_object
-
-				jogo = Jogo.new(output)
-
 				output.should_receive(:puts).with("Informe o jogador:")
 
 				jogo.iniciar
